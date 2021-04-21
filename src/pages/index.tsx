0564d6e5ad209075257cd74021152470f02737e1
Gabriel Merigo <gabriel.merigo.dev@gmail.com>
2021-04-20 21:49:54 -0300

    new asssc

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,16 @@
+// SPA
+// SSR
+// SSG
+
+import { useEffect } from "react"
+
 export default function Home() {
+  useEffect(() => {
+    fetch('http://localhost:3333/episodes')
+      .then((response) => response.json())
+      .then( response =>console.log(response))
+  }, [])
+
   return (
     <h1>index</h1>
   )
