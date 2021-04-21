5654dbd3060fbb5b6dc418bae98a5235080af647
Gabriel Merigo <gabriel.merigo.dev@gmail.com>
2021-04-21 18:08:25 -0300

    formatando dados

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,14 @@
 import { GetStaticProps } from 'next';
-import { api } from '../services/api'
+import { format, parseISO} from 'date-fns';
+import ptBR from 'date-fns/locale/pt-BR'
+import { api } from '../services/api';
+import { convertDurationToTimeString } from '../utils/convertDurationToTimeString';
 
 type Episode = {
   id: string;
   title: string;
-  members: string
+  members: string;
+  published_at: string;
 }
 
 type HomeProps = {
@@ -13,7 +17,10 @@ type HomeProps = {
 
 export default function Home(props: HomeProps) {
   return (
-    <h1>index</h1>
+    <div>
+      <h1>index</h1>
+      <p>{new Date(props.episodes[0].published_at)}</p>
+    </div>
   )
 }
 
@@ -26,9 +33,23 @@ export const getStaticProps: GetStaticProps = async () => {
     }
   })
 
+  const episodes = data.map(episode => {
+    return {
+      id: episode.id,
+      title: episode.title,
+      thumbnail: episode.thumbnail,
+      members: episode.members,
+      publishedAt: format(parseISO(episode.published_at), 'd MMM yy', {locale: ptBR}),
+      duration: Number(episode.file.duration),
+      durationAsString: convertDurationToTimeString(Number(episode.file.duration)),
+      description: episode.description,
+      url: episode.file.url,
+    }
+  })
+
   return {
     props: {
-      episodes: data,
+      episodes
     },
     revalidate: 60 * 60 * 8,
   }
