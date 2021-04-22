87f37741c5db1138cf8fa50f53f3ef48ed02f15b
Gabriel Merigo <gabriel.merigo.dev@gmail.com>
2021-04-22 19:31:07 -0300

    Update [slug].tsx

--- src/pages/episodes/[slug].tsx
+++ src/pages/episodes/[slug].tsx
@@ -2,6 +2,7 @@ import { format, parseISO } from "date-fns";
 import { ptBR } from "date-fns/locale";
 import Image from "next/image";
 import Link from 'next/link'
+import { useRouter } from 'next/router'
 import { GetStaticPaths, GetStaticProps } from "next";
 
 import { api } from "../../services/api";
@@ -26,6 +27,12 @@ type EpisodeProps = {
 };
 
 export default function Episode({ episode }: EpisodeProps) {
+  const router = useRouter();
+
+  if (router.isFallback) {
+   return <p>Carregando...</p> 
+  }
+
   return (
     <div className={styles.episode}>
       <div className={styles.thumbnailContainer}>
@@ -41,7 +48,7 @@ export default function Episode({ episode }: EpisodeProps) {
           objectFit="cover"
         />
         <button type="button">
-          <img src="/play.svg" alt="Tocar episódio"/>
+          <img src="/play.svg" alt="Tocar episódio" />
         </button>
       </div>
 
@@ -52,17 +59,39 @@ export default function Episode({ episode }: EpisodeProps) {
         <span>{episode.durationAsString}</span>
       </header>
 
-      <div 
-        className={styles.description} 
-        dangerouslySetInnerHTML={{__html: episode.description}}
+      <div
+        className={styles.description}
+        dangerouslySetInnerHTML={{ __html: episode.description }}
       />
     </div>
   );
 }
 
 export const getStaticPaths: GetStaticPaths = async () => {
+  const { data } = await api.get("episodes", {
+    params: {
+      _limit: 12,
+      _sort: "published_at",
+      _order: "desc",
+    },
+  });
+
+  const paths = data.map(episode => {
+    return {
+      params: {
+        slug: data.id
+      }
+    }
+  })
+
   return {
-    paths: [],
+    paths: [
+      {
+        params: {
+          slug: 'aplicacao-de-arquiteturas-mvc-e-clean-architecture-na-pratica'
+        }
+      }
+    ],
     fallback: "blocking",
   };
 };
